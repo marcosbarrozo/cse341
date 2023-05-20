99906a81e1d79028cf0c06e9145ad91b7b817872
Marcos Barrozo <marcos.barrozo18@gmail.comm>
2023-05-20 18:33:23 -0300

    server js

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(bodyParser.json())
 .use((req,res,next)=>{
     res.setHeader('Acess-Control-Allow-Origin','*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    
     next();
 })
 .use('/', require('./routes/'))
